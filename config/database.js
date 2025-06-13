853b53dd45b6cf425019c2eb4738178e78b34715
shaymaa-khz98 <shaymaaelkhzondar@gmail.com>
2025-06-13 23:29:25 +0300

    fix: database credentials

--- config/database.js
+++ config/database.js
@@ -14,14 +14,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql', // أو postgres حسب قاعدة بياناتك
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    }
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'mysql', // or 'mysql' or 'oracle'
   }
-});
+);
 
 module.exports = sequelize;

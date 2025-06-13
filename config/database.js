6fcf5e115c8a0b3cb9e0667958f832d3f8934dce
shaymaa-khz98 <shaymaaelkhzondar@gmail.com>
2025-06-13 23:44:18 +0300

    Fix: database connection

--- config/database.js
+++ config/database.js
@@ -12,17 +12,10 @@
 // module.exports = sequelize;
 
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: 'mysql', // or 'mysql' or 'oracle'
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  logging: false,
+});
 
 module.exports = sequelize;

f0abb619bbb26fe6555a563846b1c4b1533ee36b
shaymaa-khz98 <shaymaaelkhzondar@gmail.com>
2025-06-14 12:07:55 +0300

    fix: correct MySQL password

--- config/database.js
+++ config/database.js
@@ -11,18 +11,10 @@
 
 // module.exports = sequelize;
 
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306,
-    logging: false, // أو true لو بتحب تشوف الـ SQL
-  }
-);
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql', // أو 'postgres' حسب نوع القاعدة
+  logging: false,
+});
 
 module.exports = sequelize;
